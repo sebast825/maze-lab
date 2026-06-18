871ab113ea781e78c1a6a1750cb3ecb5a408e6f1
sebast825 <sebamolina825@gmail.com>
2026-06-18 13:58:54 -0300

    refactor: center canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,16 @@ import { useCanvasPDF } from "@/features/maze/useCanvasPDF";
 import { useMazeGenerator } from "@/features/maze/useMazeGenerator";
 import { AlgorithmType } from "@/lib/alogirthms/generation";
 import { useState, useRef } from "react";
-import { ToolBar } from "@/components/toolBar";
-import { Actions, Controls, Modes } from "@/features/maze/menu";
 import { useMazeMetrics } from "@/features/maze/useMazeMetrics";
 import benchmark20x20 from "@/lib/maze/benchmark/rawData/manual/20x20.json";
 import { MazeBenchmark } from "@/lib/maze/benchmark/types";
-import { DifficultyBadge } from "@/features/mazeDifficulty/difficultyBadge";
 import { useSafeDebouncedAction } from "@/hooks/useSafeDebouncedAction";
 import {
   MazeRawMetrics,
   MazeScoringResult,
 } from "@/lib/maze/metrics/scoring/types";
 import { MazeData } from "@/lib/maze/types";
+import { Navbar } from "@/components/navBar/index";
 
 export type GameMode = "VIEW" | "DRAW" | "CHARACTER";
 
@@ -37,9 +35,17 @@ export default function Home() {
 
   const drawingRef = useRef<DrawingCanvasRef | null>(null);
   // Absolute constant sizing configuration for grid rendering units
-  const CELL_SIZE = 25;
+  const maxWidth = window.innerWidth * 1;
+  const maxHeight = window.innerHeight * 0.75;
+  const CELL_SIZE = Math.min(
+    25, // max cap
+    Math.max(
+      15, // min cap
+      Math.min(maxWidth / cols, maxHeight / rows),
+    ),
+  );
+  console.log(CELL_SIZE);
   const run = useSafeDebouncedAction(500);
-
   const handleUndoDraw = () => {
     drawingRef.current?.undo();
   };
@@ -51,13 +57,15 @@ export default function Home() {
     const maze = createMaze(algorithm, rows, cols);
     handleClearDraw();
     setShowPath(false);
-
+    
     run(() => {
+      //this metrics is necesary to update the metrics inside hook and show difficulty do not coment
       const metrics: MazeScoringResult = calculateMetrics(maze);
+    /*    console.log("metrics:", metrics);
       // Build a complete benchmark snapshot of this maze run
       // (raw structure + metadata) for logging and later analysis
-      const rawData = buildMazeBenchmark(metrics.raw, maze, algorithm);
-      console.log("rawData ready:", rawData);
+       const rawData = buildMazeBenchmark(metrics.raw, maze, algorithm);
+      console.log("rawData ready:", rawData);*/
     });
   };
 
@@ -77,77 +85,56 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen w-full items-center justify-center bg-slate-950 font-sans md:max-h-[100vh]  px-4">
+    <div className="flex flex-col min-h-screen w-full items-center justify-center bg-slate-950 font-sans md:max-h-[100vh]  px-4 h-full">
       {/* 1. Changed max-w-3xl to max-w-full/w-full and aligned children to center */}
-      <main className="flex flex-col flex-1 w-full max-w-full items-center justify-between  my-10">
+      <main className="flex flex-col flex-1 w-full max-w-full items-center justify-between  my-10 h-full">
         {/* 2. Added centering to the direct wrapper container */}
-        <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center w-full md:h-screen ">
           {/* 3. Restricted menu to a readable reading width so it doesn't split apart */}
-          <div className="w-full max-w-3xl mb-6">
-            <ToolBar>
-              <Controls
-                algorithm={algorithm}
-                onAlgorithmChange={setAlgorithm}
-                rows={rows}
-                onRowsChange={setRows}
-                cols={cols}
-                onColsChange={setCols}
-                onGenerate={handleGenerate}
-              />
-              <Actions
-                generateMaze={handleGenerate}
-                onShowPath={() => setShowPath(!showPath)}
-                showPath={showPath}
-                exportToPDF={() => handleExportToPDF(mazeData!, cols)}
-                disableExportToPDF={!mazeData}
-              />
-
-              {mazeData && (
-                <>
-                  <Modes
-                    currentMode={gameMode}
-                    toggleCharacter={() =>
-                      gameMode != "CHARACTER"
-                        ? setGameMode("CHARACTER")
-                        : setGameMode("VIEW")
-                    }
-                    toggleDraw={() =>
-                      gameMode != "DRAW"
-                        ? setGameMode("DRAW")
-                        : setGameMode("VIEW")
-                    }
-                    undoLast={() => handleUndoDraw()}
-                    clearAll={() => handleClearDraw()}
-                    drawCanvas={gameMode == "DRAW"}
-                  />
-                </>
-              )}
-              {metrics?.scores.total && (
-                <DifficultyBadge score={metrics.scores.total}></DifficultyBadge>
-              )}
-            </ToolBar>
-          </div>
-          <div className="relative w-full  overflow-auto border border-black rounded bg-slate-950 ">
+          <Navbar
+            algorithm={algorithm}
+            setAlgorithm={setAlgorithm}
+            rows={rows}
+            setRows={setRows}
+            cols={cols}
+            setCols={setCols}
+            handleGenerate={handleGenerate}
+            showPath={showPath}
+            setShowPath={setShowPath}
+            handleExportToPDF={handleExportToPDF}
+            mazeData={mazeData}
+            gameMode={gameMode}
+            setGameMode={(e) => setGameMode(e)}
+            handleUndoDraw={handleUndoDraw}
+            handleClearDraw={handleClearDraw}
+            total={metrics?.scores.total}
+          />
+          <div className="relative w-full  overflow-auto border border-black rounded h-full">
             <div className="grid min-h-full min-w-full place-items-center">
-              <div className="w-fit relative min-h-full">
-                {mazeData && mazeData.end && (
-                  <MazeCanvas
-                    mazeData={mazeData}
-                    cellSize={CELL_SIZE}
-                    showPath={showPath}
-                  />
-                )}
-                {gameMode == "CHARACTER" && mazeData && (
-                  <CharacterCanvas mazeData={mazeData!} cellSize={CELL_SIZE} />
-                )}
-                {gameMode == "DRAW" && (
-                  <DrawingCanvas
-                    cols={cols}
-                    rows={rows}
-                    cellSize={CELL_SIZE}
-                    ref={drawingRef}
-                  />
-                )}
+              <div className="w-fit relative min-h-full grid place-items-center">
+                <div className="relative">
+                  {mazeData && mazeData.end && (
+                    <MazeCanvas
+                      mazeData={mazeData}
+                      cellSize={CELL_SIZE}
+                      showPath={showPath}
+                    />
+                  )}
+                  {gameMode == "CHARACTER" && mazeData && (
+                    <CharacterCanvas
+                      mazeData={mazeData!}
+                      cellSize={CELL_SIZE}
+                    />
+                  )}
+                  {gameMode == "DRAW" && (
+                    <DrawingCanvas
+                      cols={cols}
+                      rows={rows}
+                      cellSize={CELL_SIZE}
+                      ref={drawingRef}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
